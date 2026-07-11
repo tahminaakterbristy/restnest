3b6ca647406a2b0fbdb76405d3c22d7410564193
tahminaakterbristy <tahminabristybd@gmail.com>
2026-07-11 21:26:23 +0600

    swagger section done

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import routes from "./app/routes";
-
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./config/swagger";
 import globalErrorHandler from "./app/errors/globalErrorHandler";
 
 dotenv.config();
@@ -16,6 +17,11 @@ app.use(express.json());
 
 app.use("/api/v1", routes);
 
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+);
 
 app.get("/", (req, res) => {
   res.send("RentNest API Running");
